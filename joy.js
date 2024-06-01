5d88307bb7fc8d59af174e13eb1a003636adf7ea
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-06-01 16:04:29 +0200

    0 Fortschritt wieder zurück am AnfNG

--- joy.js
+++ joy.js
@@ -2,8 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     const jstripe = document.getElementById("jstripe");
-    const jbagel = document.getElementById("jbagel");
-    var SFBagelClick = 1;
+
 
     document.addEventListener("scroll", scrollingtest);
     
@@ -12,19 +11,11 @@ document.addEventListener('DOMContentLoaded', function () {
     //SF für Scroll Fortschritt
     function scrollingtest() {
         var SF = scrollY;
-        var SFBagel = (scrollY - 1050) * SFBagelClick - 950;
         jstripe.style.transform = "translate(" + -SF + "px , " + SF + "px)";
-        jbagel.style.transform = "translate(" + -SFBagel + "px , " + SF + "px)";
         console.log("Scrollfortschritt: "+SF);
         if (SF > 7000) {
             window.open("https://remohoesli.github.io/everything-everywhere/evelyn.html", "_self");
         }
-
-        document.getElementById("jbagel").addEventListener("click", function() {
-            SFBagelClick = 0;
-            jbagel.style.transform = "translate(" + 950 + "px , " + SF + "px) scale("+  +")";
-            console.log("Bagel clicked!");
-        });
     }
 
     

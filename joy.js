ac142a5d876f50f4cc8c44ab55a3bf5e71da5aaf
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-06-05 15:17:37 +0200

    straight scroll joy

--- joy.js
+++ joy.js
@@ -2,23 +2,21 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     const container = document.getElementById("container");
-    const jstripe = document.getElementById("jstripe");
-    const widebagel = document.getElementById("widebagel");
 
 
     document.addEventListener("scroll", scrollingtest);
     var SF = 0;
-    var SFwidebagel = 1;
 
 
   
     //inst for future self: id individual, progress global, horizontal scroll = -scrollY, vertical scroll = scrollY
     //SF für Scroll Fortschritt
     function scrollingtest() {
-        if (SF < 2600) {
+        if (SF < 20000) {
             SF = scrollY;
             container.style.transform = "translate(" + -SF + "px , " + SF + "px)";
         }
+        /*
         //Stopping for Bagel
         else if (SF < 4600) {
             SF = scrollY;
@@ -30,7 +28,7 @@ document.addEventListener('DOMContentLoaded', function () {
             SF = scrollY;
             window.open("https://remohoesli.github.io/everything-everywhere/evelyn.html", "_self");
         }
-       /* if (SF < 10000) {
+        if (SF < 10000) {
             SF = scrollY;
             var SF_1 = SF - 2000;
             container.style.transform = "translate(" + -SF_1 + "px , " + SF + "px)";

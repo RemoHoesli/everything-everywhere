bda8a7ad65b2faa003a193218f59b71207206d58
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-06-05 14:56:43 +0200

    scrolling interactions

--- evelyn.js
+++ evelyn.js
@@ -104,10 +104,73 @@ document.addEventListener('DOMContentLoaded', function () {
                 document.getElementById("sentimental").style.transform = "translate(" + 3650 + "px , " + -100 + "px)";
             }
         }
-        else if (SF < 30000) {
+        else if (SF < 27300) {
             SF = scrollY;
             var SF_2 = SF - 11800;
             container.style.transform = "translate(" + -SF_2 + "px , " + SF + "px)";
+            if (SF > 22500) {
+                document.getElementById("fightf2").style.opacity = "1";
+            }
+            if (SF > 25500) {
+                document.getElementById("fightf5").style.opacity = "1";
+                document.getElementById("fightf5").style.transform = "translate(" + 7350 + "px , " + 200 + "px)";
+            }
+        }
+        else if (SF < 30500) {
+            SF = scrollY;
+            container.style.transform = "translate(" + -15500 + "px , " + SF + "px)";
+            if (SF > 27500) {
+                document.getElementById("hotdoghdsmoke").style.opacity = "1";
+                document.getElementById("hotdoghd1").style.transform = "translate(" + 8200 + "px , " + -900 + "px)";
+            }
+            if (SF > 27800) {
+                document.getElementById("hotdoghdfont1").style.opacity = "0";
+                document.getElementById("hotdoghd1").style.transform = "translate(" + 8200 + "px , " + 0 + "px)";
+            }
+            if (SF > 28000) {
+                document.getElementById("hotdoghdfont1").style.opacity = "1";
+                document.getElementById("hotdoghdfont2").style.opacity = "0";
+            }
+            if (SF > 28200) {
+                document.getElementById("hotdoghdfont2").style.opacity = "1";
+                document.getElementById("hotdoghdfont3").style.opacity = "0";
+            }
+            if (SF > 28400) {
+                document.getElementById("hotdoghdfont3").style.opacity = "1";
+                document.getElementById("hotdoghdfont4").style.opacity = "0";
+            }
+            if (SF > 28600) {
+                document.getElementById("hotdoghdfont4").style.opacity = "1";
+                document.getElementById("hotdoghdfont5").style.opacity = "0";
+            }
+            if (SF > 28800) {
+                document.getElementById("hotdoghdfont5").style.opacity = "1";
+                document.getElementById("hotdoghdfont6").style.opacity = "0";
+            }
+            if (SF > 29000) {
+                document.getElementById("hotdoghdfont6").style.opacity = "1";
+            }
+        }
+        else if (SF < 34000) {
+            SF = scrollY;
+            var SF_3 = SF - 15000;
+            container.style.transform = "translate(" + -SF_3 + "px , " + SF + "px)";
+
+            document.getElementById("hotdogbutton3").addEventListener("click", function() {
+                document.getElementById("hotdoghd3").style.animationName = "hddance";
+            });
+        }
+        else if (SF > 34000) {
+            SF = scrollY;
+            container.style.transform = "translate(" + -19350 + "px , " + SF + "px)";
+
+            document.getElementById("hotdoghd02").addEventListener("click", function() {
+                document.getElementById("hotdoghd02").style.transform = "translate(" + 10650 + "px , " + -350 + "px) scale(0.8)";
+                console.log("clicked!");
+            });
+            document.getElementById("hotdoghd01").addEventListener("click", function() {
+                document.getElementById("hotdoghd01").style.transform = "translate(" + 10600 + "px , " + -800 + "px) scale(0.8)";
+            });
         }
 
 

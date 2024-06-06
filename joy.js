bf31cb744e8ad81b08b20c7cf2f0db9d39a6b1d9
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-06-06 15:05:51 +0200

    Screen height responsiveness Joy page

--- joy.js
+++ joy.js
@@ -2,16 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     const container = document.getElementById("container");
+    const responsive = document.getElementById("responsive");
 
-
+    
     document.addEventListener("scroll", scrollingtest);
     var SF = 0;
 
-
+    var scalefactor = window.innerHeight/980;
+    responsive.style.transform = "translate(" + 0 + "px , " + -SF*(scalefactor-1) + "px) scale("+ scalefactor +")";
+    
   
     //inst for future self: id individual, progress global, horizontal scroll = -scrollY, vertical scroll = scrollY
     //SF für Scroll Fortschritt
     function scrollingtest() {
+        responsive.style.transform = "translate(" + 0 + "px , " + -SF*(scalefactor-1) + "px) scale("+ scalefactor +")";
         if (SF < 20000) {
             SF = scrollY;
             container.style.transform = "translate(" + -SF + "px , " + SF + "px)";

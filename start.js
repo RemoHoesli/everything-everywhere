785df7cace22f1fa3de309a2f94611f17e1a1d82
RemoHoesli <remo.hoesli@stud.hslu.ch>
2024-06-07 09:18:26 +0200

    waymondsiite hinzue

--- start.js
+++ start.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
 
     document.getElementById("box1").addEventListener("click", function() {
-        destinationArray = ["https://remohoesli.github.io/everything-everywhere/evelyn.html", "https://remohoesli.github.io/everything-everywhere/waymond.html", "https://remohoesli.github.io/everything-everywhere/joy.html"];
+        destinationArray = ["https://remohoesli.github.io/everything-everywhere/evelyn.html", "https://blublex.github.io/DIDES_WaymondWang/", "https://remohoesli.github.io/everything-everywhere/joy.html"];
         var num = Math.floor(Math.random() * destinationArray.length);
         document.getElementById("box1").href = destinationArray[num];
     });    
